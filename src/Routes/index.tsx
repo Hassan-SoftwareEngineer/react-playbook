a435a125c8bdeaaab6f81d7da108f86a9a2a7c6a
Hassan Javed <hassan.javed.softwareengineer@gmail.com>
2025-06-02 04:08:43 +0500

    setting up routes

---
+++ src/Routes/index.tsx
@@ -0,0 +1,22 @@
+import About from "@/pages/About";
+import Home from "@/pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path : "/",
+        element : <Home />
+    },
+    {
+        path : "/about",
+        element : <About />
+    },
+])
+
+const Routes = () => {
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default Routes;
